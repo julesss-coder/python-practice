e378f6426425c3a0dc2cacbc269f69b89c9f6685
julesss-coder <julesss@posteo.de>
2025-02-05 18:36:51 +0100

    WIP

--- python_practice.py
+++ python_practice.py
@@ -1,8 +1,10 @@
 # 7.8., 8:30 - 10:00
 # 22.1.2025, 8:34 - 9:04, mark 23:54
 # 22.1.2025, 18:02 - 18:52, mark 47:40
-# 29.1.2025, 8:00 - 9:00, mark 59:39
-# 29.1.2025, 16:34
+# 29.1.2025, 8:00 - 9:00, mark 59:39 
+# 29.1.2025, 16:34 - 17:52 - mark 1:21:13
+# 5.2.2025, 8:50-9:37, mark 1:28:45
+# 5.2.2025, 17:48 - , mark 1:51:03
 # Following along to tutorial "Object Oriented Programming with Python - Full Course for Beginners", URL: https://www.youtube.com/watch?v=Ej_02ICOIgs&t=4402s
 # Continue at mark 1:14:00
 
@@ -15,7 +17,6 @@ class Item:
   pay_rate = 0.8
   all = []
 
-
   # When you instantiate a class, Python calls __init__() method automatically
   # Use a default parameter if you don't always have objects of this type
   # Specify a data type for each parameter. (Passing in a default value signifies that the given data type is expected.)
@@ -26,12 +27,21 @@ class Item:
     assert quantity >= 0, f"Quantity {quantity} is not greater than 0!"
 
     # Assign arguments to self object
-    self.name = name
+    self.__name = name
     self.price = price
     self.quantity = quantity
 
     # Actions to execute
     Item.all.append(self)
+
+
+  @property
+  def name(self):
+    return self.__name.upper()
+
+  @name.setter
+  def set_name(self, name):
+      self.__name = name
 
   # When you call this method, Python passes the instance of the class to the method as the first argument. So, ALL methods must at least have the parameter `self`
   def calculate_total_price(self):
@@ -81,8 +91,13 @@ class Item:
 
 # print(Item.is_integer('a'))
 # print(Item('laptop', 10, 10).is_integer('a'))
-print(Item(3, 22.2, 3.3))
-print(Item.all)
+# print(Item(3, 22.2, 3.3))
+# print(Item.all)
+phone = Item('phone', 20, 5)
+# print(phone.__name) #erw.: error
+phone.set_name = 'new name' 
+print(phone.name) #erw 'new name'
+
 
 
 class Person:
@@ -96,3 +111,16 @@ julia = Person()
 # print(Person.name) #exp: error => reality: AttributeError: type object 'Person' has no attribute 'name'
 # print(julia.__dict__) #Exp. prints instance attributes, i.e. name; reality: {'name': 'Julia'}
 # print(Person.__dict__) #Exp. prints class attributes, i.e. sex; reality: {'__module__': '__main__', 'sex': 'female', '__init__': <function Person.__init__ at 0x78d0728a6520>, '__dict__': <attribute '__dict__' of 'Person' objects>, '__weakref__': <attribute '__weakref__' of 'Person' objects>, '__doc__': None}
+
+
+# print(dir(Item))
+
+class Phone(Item):
+  pass
+
+# print(Phone('test item', 20, 1))
+
+# print(Phone.__class__)
+# print(Phone.__class__.__name__)
+
+
